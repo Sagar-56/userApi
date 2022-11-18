b0fad2904ec68543a2243da4df9dc3ea19f3c738
Sagar-56 <sb0473564@gmail.com>
2022-11-18 13:32:47 +0530

    make it better

--- configs/config.js
+++ configs/config.js
@@ -6,8 +6,8 @@ const config = {
     },
     default: {
         SECRET: 'mysecretkey',
-        // DATABASE: 'mongodb://localhost:27017/eduInternJan'
-        DATABASE: 'mongodb+srv://Sagarbehera:Sagar456@cluster0.96hmj.mongodb.net/eduInternJan?retryWrites=true&w=majority'
+        DATABASE: 'mongodb://localhost:27017/eduInternJan'
+        // DATABASE: 'mongodb+srv://Sagarbehera:Sagar456@cluster0.96hmj.mongodb.net/eduInternJan?retryWrites=true&w=majority'
     }
 }
 exports.get = function get(env) {
