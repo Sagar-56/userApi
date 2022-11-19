6d5f1fdb5d2881f56e90f6d5cb15a4b780c31aad
Sagar-56 <sb0473564@gmail.com>
2022-11-19 13:37:23 +0530

    make it better

--- models/users.js
+++ models/users.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcryptjs = require('bcryptjs');
 const confiq = require('../configs/config').get(process.env.NODE_ENV);
 const salt = 10;
 
@@ -42,10 +42,10 @@ userSchema.pre('save', function (next) {
     var user = this;
 
     if (user.isModified('password')) {
-        bcrypt.genSalt(salt, function (err, salt) {
+        bcryptjs.genSalt(salt, function (err, salt) {
             if (err) return next(err);
 
-            bcrypt.hash(user.password, salt, function (err, hash) {
+            bcryptjs.hash(user.password, salt, function (err, hash) {
                 if (err) return next(err);
                 user.password = hash;
                 user.password2 = hash;
@@ -63,7 +63,7 @@ userSchema.pre('save', function (next) {
 
 
     userSchema.methods.comparePassword = function (password, cb) {
-        bcrypt.compare(password, this.password, function (err, isMatch) {
+        bcryptjs.compare(password, this.password, function (err, isMatch) {
             if (err) return cb(err);
             cb(null, isMatch);
         })
