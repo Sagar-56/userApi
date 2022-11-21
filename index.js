98cdf211d215aa2e02d1b0e272cb2750393b5c34
Sagar-56 <sb0473564@gmail.com>
2022-11-21 16:33:12 +0530

    make it better

--- index.js
+++ index.js
@@ -38,18 +38,10 @@ MongoClient.connect(DATABASE, (err, client) => {
 
 // database connection
 mongoose.Promise = global.Promise;
-mongoose.connect(db.DATABASE, { useNewUrlParser: true, useUnifiedTopology: true })
-// , function (err) {
-//     if (err) console.log(err);
-//     console.log("database is connected");
-// })
-.then((err)=>{
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }, function (err) {
     if (err) console.log(err);
     console.log("database is connected");
-  }).catch(error => {
-    console.log("mongo error",error);
-  })
-
+})
 
 
 app.get('/', function (req, res) {
