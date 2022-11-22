fae9d201ea6ae4cea822bb7a0aa8ab4b6e400f1c
Sagar-56 <sb0473564@gmail.com>
2022-11-22 12:40:29 +0530

    make it better

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const cors = require('cors')
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cookieParser());
 app.use(cors());
